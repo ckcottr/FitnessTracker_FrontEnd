4ade98b1fb5742c08c675ccf740e4fb4021c6223
Clayton <claytonKcottrell@Outlook.com>
2021-10-05 19:12:03 -0500

    added a build in package.json

--- src/components/home.js
+++ src/components/home.js
@@ -6,14 +6,8 @@ import ReactDOM from 'react-dom';
 const Home = () =>{
     return(
         <div id = "homepage">
-<<<<<<< HEAD
-        <h1 id="welcome">Welcome to Fitness Tracker!</h1>
-        <p id="body">This website is designed to help you with your fitness goals!</p>
-        <p id="body">Please Login to continue.</p>
-=======
         <h1 id="home">Welcome to Fitness Tracker!</h1>
         <p id="body">This website is designed to help you with your fitness goals!</p>
->>>>>>> b34b5b3ead7539014ce3b6b5a0996f76e8a04d8c
         </div>
     )
 }
