c0b6a4a1a5024bff952be2214806454d7052f0e9
M'ary <m_aryc27@yahoo.com>
2021-10-27 22:19:06 -0500

    start of new updates

--- src/components/activities.js
+++ src/components/activities.js
@@ -1,39 +1,73 @@
-import React, { useEffect, useState } from 'react'
-import ReactDOM from 'react-dom'
+import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import API from '../api/api';
 
-const activities = ({activitiesList, makeActivitiesList}) => {
-    useEffect( function () {
-        fetch('https://fitnesstrac-kr.herokuapp.com/api/activities')
-               .then(response => response.json())
-               .then(result => {
-                   console.log(result);
-                   makeActivitiesList(result)
-                   
-               })
-               .catch(function (error){
-                   console.log("something")
-                   console.error(error)
-               }    )
-   
-       }, []);
-       console.log(activitiesList);
-       const activitiesElement = activitiesList.map((activities) =>
-           <div id="allActivities">
-               <h1>Activity#: {activities.id}</h1>
-               <h2>Name: {activities.name}</h2>
-               <h3>Description: {activities.description}</h3>
-               
-           </div>
-   
-   
-       );
-   
-    return(
+const activities = ({isLoggedIn}) => {
+    const [activities, setActivities] = useState([]);
+    const [render, setRender] = useState ('');
+    useEffect(async function() {
+        try {
+            const data = await API.makeRequest('/activities', GET);
+            console.log(data);
+            setActivities(data);
+        } catch (error) {
+            alert(error);
+        }
+    }, [render]);
+
+    const activityElements = activities.map((activity, i) => 
+        <div className='activity-container' key={`activity-id-${i}`}>
+            <p>{activity.name}</p>
+            <p>{activity.description}</p>
+        </div>);
+
+    return (
         <div>
-            <h1 id="actheader">Activities</h1>
-            {activitiesElement}
-        </div>
+            {isLoggedIn?}
+            <>
+                <makeActivities setRender={setRender}/>
+                <h2 className='title'>All Activities</h2>
+                <div className='activities-container'>{activityElements}</div>
+            </>
+            </div>
     )
-}
+        }
 
-export default activities
+
+// const activities = ({activitiesList, makeActivitiesList}) => {
+//     useEffect( function () {
+//         fetch('https://fitnesstrac-kr.herokuapp.com/api/activities')
+//                .then(response => response.json())
+//                .then(result => {
+//                    console.log(result);
+//                    makeActivitiesList(result)
+                   
+//                })
+//                .catch(function (error){
+//                    console.log("something")
+//                    console.error(error)
+//                }    )
+   
+//        }, []);
+//        console.log(activitiesList);
+//        const activitiesElement = activitiesList.map((activities) =>
+//            <div id="allActivities">
+//                <h1>Activity#: {activities.id}</h1>
+//                <h2>Name: {activities.name}</h2>
+//                <h3>Description: {activities.description}</h3>
+               
+//            </div>
+   
+   
+//        );
+   
+//     return(
+//         <div>
+//             <h1 id="actheader">Activities</h1>
+//             {activitiesElement}
+//         </div>
+//     )
+// }
+
+// export default activities
--- src/index.js
+++ src/index.js
@@ -1,52 +1,12 @@
-import ReactDOM from 'react-dom';
-import React, { useState } from 'react';
-import Login from './components/login';
-import Home from './components/home'
-import Profile from './components/profile'
-import Navbar from './components/navbar';
-import Register from './components/register'
-import Routines from './components/routines'
-import Activities from './components/activities'
-import { BrowserRouter as Router, Route, Switch, Link, Redirect } from "react-router-dom"
-import './components/style/home.css'
+import React from 'react';
+import { ReactDOM } from 'react-dom';
+import {
+    BrowserRouter as router
+} from 'react-router-dom';
 
+import App from './components/App';
 
-const App = () => {
-    const [isNewUser, setIsNewUser] = useState(true)
-    const renderAuthForm = (e) => {
-        if (isNewUser) {
-            return <Login toggleNewUser={setIsNewUser} />
-        } else {
-            return <Register toggleNewUser />
-        }
-    }
-    
-    return (
-        <>
-            <Router>
-                <div id="navbar">
-                    <Navbar />
-                </div>
-                <main>
-                    <Switch>
-                        <div>
-                            <Route exact path="/login"><Login /></Route>
-                            <Route exact path="/activities"><Activities/></Route>
-                            <Route exact path="/profile"><Profile/></Route>
-                            <Route exact path="/register"><Register /></Route>
-                            <Route exact path="/routines"><Routines /></Route>
-                            <Route exact path="/"><Home /></Route>
-                        </div>
-                    </Switch>
-                </main>
-            </Router>
-
-
-
-        </>
-    )
-}
-
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router><App /></Router>,
+    document.getElementById('app')
+);
